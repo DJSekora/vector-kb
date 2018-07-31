397e07aa9879d83afc8476bfc2a10ea191a9d297
summerstay <jsummerstay@gmail.com>
2018-07-31 13:47:17 -0400

    Add files via upload

--- spvscript.m
+++ spvscript.m
@@ -1,11 +1,12 @@
-term='truck';
+term='llama';
 no_of_sentences=10;
 [output_vector1, tail_weights, tail_inds] = simplexQAunlimited(str2vec(term,h3,alphabetized_words,word_index),'hasSubject',conceptnet.relations,sentenceword,h3,index,word);
 [output_vector2, tail_weights, tail_inds] = simplexQAunlimited(str2vec(term,h3,alphabetized_words,word_index),'hasPredicate',conceptnet.relations,sentenceword,h3,index,word);
 [output_vector3, tail_weights, tail_inds] = simplexQAunlimited(str2vec(term,h3,alphabetized_words,word_index),'hasObject',conceptnet.relations,sentenceword,h3,index,word);
-word1=vec2str(output_vector1,h3,word,no_of_sentences);
+not_someone=-str2vec('someone', h3, alphabetized_words, word_index);
+word1=vec2str(output_vector1+.4*not_someone,h3,word,no_of_sentences);
 word2=vec2str(output_vector2,h3,word,no_of_sentences);
-word3=vec2str(output_vector3,h3,word,no_of_sentences);
+word3=vec2str(output_vector3+.4*not_someone,h3,word,no_of_sentences);
 
 for ii=1:no_of_sentences
 fprintf('%s %s %s.\n',word1{ii},word2{ii},word3{ii});
