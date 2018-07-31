8e1731e78fc009dfa1382fa89cda36704d216cdb
summerstay <jsummerstay@gmail.com>
2018-07-31 07:03:18 -0400

    Add files via upload
    These have changed recently

--- best_path2.m
+++ best_path2.m
@@ -20,9 +20,9 @@ end
 
 first_indices=db.fti1(relation_list);
 second_indices=db.fti2(relation_list);
-first_points = h3(:,first_indices);
-second_points = h3(:,second_indices);
-unique_indices=unique([first_indices, second_indices]);
+near_end_indices=vec2ind(vec2,h3,50);
+unique_indices=unique([first_indices, second_indices, near_end_indices']);
+%unique_indices=unique([first_indices(1), second_indices(1), near_end_indices']);
 %costs=unique_relation.fcost(full_summed_relation_list);
 points=[h3(:,unique_indices),vec1,vec2];
 %finds the actual distance between all head and tail terms
@@ -50,7 +50,6 @@ solutions=0;
 solution_limit=1000;
 while solutions<solution_limit
     solutions=solutions+1;
-    
     count=1;
     for jj=1:size(dists,1)
         for kk=1:size(dists,2)
@@ -72,27 +71,61 @@ while solutions<solution_limit
     
     term_graph=digraph(starts,ends,weights);
     
-    %search the graph for the shortest path
+    %search the graph for the lowest cost path
     path =shortestpath(term_graph,size(points,2)-1,size(points,2));
     pathend=path(end-1);
-    fprintf('%');
     
+    %print the lowest cost path
+    total_cost=0;
     if size(path,2)>3
-        fprintf('Start %.3f->',dists(path(1),path(2)));
         for pathstep=2:size(path,2)-2
             aa=find(first_indices==unique_indices(path(pathstep)));
             bb=find(second_indices==unique_indices(path(pathstep+1)));
             current_relation=intersect(aa,bb);
             if size(current_relation,2)>0
+                current_relation=current_relation(1);
+                if pathstep==2
+                    fprintf('Start %.3f->',dists(path(1),path(2)));
+                    total_cost=total_cost+dists(path(1),path(2));
+                end
                 relation_name=relation{db.ftir(relation_list(current_relation))};
-                head_term=word{db.fti1(relation_list(current_relation))};
-                tail_term=word{db.fti2(relation_list(current_relation))};
+                %head_term=word{db.fti1(relation_list(current_relation))};
+                %tail_term=word{db.fti2(relation_list(current_relation))};
+                head_term=word{unique_indices(path(pathstep))};
+                tail_term=word{unique_indices(path(pathstep+1))};
                 ft_index_list(pathstep)=relation_list(current_relation);
-                dist=dists(path(pathstep),path(pathstep+1));
-                fprintf('%s|%s %.3f ->',head_term,relation_name,dist);
+                fprintf('%s|%s ',head_term,relation_name);
+            else
+                if pathstep==2
+                    fprintf('Start %.3f-> %s ',dists(path(1),path(2)),tail_term);
+                    total_cost=total_cost+dists(path(1),path(2));
+                else
+                    fprintf(' %s ',tail_term);
+                    tail_term=word{unique_indices(path(pathstep+1))};
+                end
             end
+            dist=dists(path(pathstep),path(pathstep+1));
+            fprintf('%.3f ->',dist);
+            total_cost=total_cost+dist;
+            if pathstep==size(path,2)-2
+                fprintf('%s %.3f->End\n',tail_term,dists(path(end-1),path(end)));
+                total_cost=total_cost+dists(path(end-1),path(end));
+                fprintf('Answer:%s total cost:%.3f\n\n',tail_term,total_cost);
+            end
+            
         end
-        fprintf('%s %.3f->End\n',tail_term,dists(path(end-1),path(end)));
+        
+        if total_cost>600
+            solutions=solution_limit;
+        end
+    else
+        tail_term=word{unique_indices(path(2))};
+        fprintf('Start->%.3f %s %.3f->End\n',dists(path(1),path(2)),tail_term,dists(path(2),path(3)));
+        total_cost=total_cost+dists(path(1),path(2))+dists(path(2),path(3));
+        fprintf('Answer:%s total cost:%.3f\n\n',tail_term,total_cost);
+        if total_cost>600
+            solutions=solution_limit;
+        end
     end
 end
 
