94f91360cf18e0f9e1a3237f62a6f5277a3404e9
summerstay <jsummerstay@gmail.com>
2018-07-31 07:00:39 -0400

    Delete add_fact_to_db.m
    This doesn't update the vector if one of the terms is unknown. Just use the parsing script.

--- add_fact_to_db.m
+++
@@ -1,8 +0,0 @@
-function [db]=add_fact_to_db(head_term,relation_term,tail_term,db,h3,alphabetized_words,word_index)
-db_size=size(db.fti1,2);
-db.fti1(db_size+1)=str2ind(head_term,alphabetized_words,word_index);
-db.fti2(db_size+1)=str2ind(tail_term,alphabetized_words,word_index);
-IndexC = strcmp(db.relation, relation_term);
-relation = find(IndexC==1);
-db.ftir(db_size+1)=relation;
-db.relation_h3=h3(:,db.fti2(:,:))-h3(:,db.fti1(:,:));
