4a553665a95cb86699d20338431b7535fba0622d
summerstay <jsummerstay@gmail.com>
2018-10-03 10:06:33 -0400

    added some informal error checks

--- add_fact_to_db.m
+++ add_fact_to_db.m
@@ -5,11 +5,18 @@ db.fti2(db_size+1)=str2ind(tail_term,alphabetized_words,word_index);
 
 IndexC = strcmp(db.relations, relation_term);
 relation = find(IndexC==1);
-db.ftir(db_size+1)=relation;
-if db.fti1(db_size+1)==0 || db.ftir(db_size+1)==0 || db.fti2(db_size+1)==0
-    fprintf('Fact not added due to unknown word: %s %s %s\n',head_term,relation_term,tail_term);
+if size(relation,2)==0
+    fprintf('Fact not added due to unknown relation: %s\n',relation_term);
     db.fti1=db.fti1(1:db_size);
     db.ftir=db.ftir(1:db_size);
     db.fti2=db.fti2(1:db_size);
+else    
+    db.ftir(db_size+1)=relation;
+    if db.fti1(db_size+1)==0 || db.ftir(db_size+1)==0 || db.fti2(db_size+1)==0
+        fprintf('Fact not added due to unknown word: %s %s %s\n',head_term,relation_term,tail_term);
+        db.fti1=db.fti1(1:db_size);
+        db.ftir=db.ftir(1:db_size);
+        db.fti2=db.fti2(1:db_size);
+    end
 end
 
--- parse_AMR_triples.m
+++ parse_AMR_triples.m
@@ -1,3 +1,43 @@
+%% Initialize variables.
+filename = 'D:\matlab apps\AMR.txt';
+delimiter = ',';
+
+%% Read columns of data as text:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%q%q%q%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to the format.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string',  'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric text to numbers.
+% Replace non-numeric text with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+
+%% Split data into numeric and string columns.
+rawNumericColumns = {};
+rawStringColumns = string(raw(:, [1,2,3]));
+
+
+%% Create output variable
+AMR = raw;
+
+%% Clear temporary variables
+clearvars filename delimiter formatSpec fileID dataArray ans raw col numericData rawNumericColumns rawStringColumns;
+
 % strip quotes
 % strip periods
 % strip -01, -02, etc from verbs (until you can get a version of word2vec
@@ -12,17 +52,18 @@ db.relations{1,88}='agent';
 db.relations{1,89}='patient';
 db.relations{1,90}='recipient';
 db.relations{1,91}='mod';
-db.relations{1,92}='op';
+db.relations{1,92}='operator';
 db.relations{1,93}='name';
 
-for ii=1:14
+for ii=1:size(AMR,1)
     fprintf('%d ',ii);
     for jj=1:3
-        current_word=AMR{ii,jj};
+        current_word=char(AMR{ii,jj});
         current_word=strrep(current_word,'ARG0','agent');
         current_word=strrep(current_word,'ARG1','patient');
         current_word=strrep(current_word,'ARG2','recipient');
         current_word=strrep(current_word,'have-rel-role','related');
+        current_word=strrep(current_word,'op','operator');
         current_word=regexprep(current_word,'[".- \d]','');
         if regexp(current_word,'/')
             current_word=strrep(current_word,'/','-');
@@ -53,5 +94,6 @@ for ii=1:14
        
         AMR2{ii,jj}=current_word;
     end
+    [alphabetized_words, word_index]=sort(word);
     [db]=add_fact_to_db(AMR2{ii,1},AMR2{ii,2},AMR2{ii,3},db,h3,alphabetized_words,word_index);
 end
